c3706c65ac996d2daf958507388fd75ac7201bac
nhyne <nhyne@nhyne.dev>
2024-09-17 22:20:26 -0400

    super basic queue that's just wrapping std struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,44 @@
+use std::collections::VecDeque;
+use std::fmt;
+use std::fmt::Formatter;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut firstQueue = SimpleQueue{queue: VecDeque::new()};
+
+    firstQueue.enqueue(3);
+    println!("first size {}",firstQueue.size());
+    firstQueue.enqueue(4);
+    println!("second size {}",firstQueue.size());
+
+    let v = firstQueue.dequeue();
+    println!("firs pop {:?}",v);
+
+    println!("third size {}",firstQueue.size());
+
+}
+
+struct SimpleQueue {
+    queue: VecDeque<u32>
+}
+
+impl Queue for SimpleQueue {
+    fn enqueue(&mut self, num: u32) -> () {
+        self.queue.push_back(num)
+    }
+
+    fn dequeue(&mut self) -> Option<u32> {
+        self.queue.pop_front()
+    }
+
+    fn size(&self) -> u32 {
+        VecDeque::len(&self.queue) as u32
+    }
+}
+
+trait Queue {
+    fn enqueue(&mut self, num: u32) -> ();
+    fn dequeue(&mut self) -> Option<u32>;
+    fn size(&self) -> u32;
 }
 
