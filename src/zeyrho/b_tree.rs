dc81158c1a6c901de2da2966cfddfdb9f55dd816
nhyne <nhyne@nhyne.dev>
2024-12-26 13:28:16 -0500

    keep getting stack overflow :(

--- src/zeyrho/b_tree.rs
+++ src/zeyrho/b_tree.rs
@@ -1,18 +1,20 @@
 use std::cell::RefCell;
+use std::panic::panic_any;
 use std::rc::Rc;
+use tonic::codegen::tokio_stream::StreamExt;
 
 const DEGREE: usize = 3;
 
 #[derive(Debug, Clone)]
 enum Node<K: Ord + std::fmt::Debug, V: std::fmt::Debug> {
     Leaf {
-        key_vals: Vec<Option<(Rc<K>, V)>>,
+        key_vals: Vec<(Rc<K>, V)>,
         next: Option<Rc<RefCell<Node<K, V>>>>,
         prev: Option<Rc<RefCell<Node<K, V>>>>,
     },
     Link {
-        separators: Vec<Option<Rc<K>>>,
-        children: Vec<Option<Rc<RefCell<Node<K, V>>>>>,
+        separators: Vec<Rc<K>>,
+        children: Vec<Rc<RefCell<Node<K, V>>>>,
     },
 }
 
@@ -39,12 +41,38 @@ impl<K: Ord + std::fmt::Debug, V: std::fmt::Debug> Node<K, V> {
 
     fn new_leaf_with_kv(key: Rc<K>, value: V) -> Self {
         let mut vec = Vec::new();
-        vec.push(Some((key, value)));
+        vec.push((key, value));
 
         Node::Leaf {
             key_vals: vec,
             next: None,
             prev: None,
+        }
+    }
+
+    fn split_leaf_node(&mut self, link_to_self: &Rc<RefCell<Self>>) -> Self {
+        if let Node::Leaf {key_vals, next, prev} = self {
+           let mid = key_vals.len() / 2;
+            println!("{:?}", key_vals);
+            let new_node = Rc::new(RefCell::new(Node::new_leaf()));
+
+            let mut new_keys_padded = key_vals.split_off(mid);
+
+            let new_node_separator = new_keys_padded.last().unwrap().0.clone();
+            if let Node::Leaf { key_vals: new_keys, next: new_next, prev: new_prev } = &mut *new_node.borrow_mut() {
+                *new_keys = new_keys_padded;
+                *new_next = next.take();
+                *new_prev = Some(Rc::clone(link_to_self));
+            }
+
+            *next = Some(Rc::clone(&new_node));
+
+            Node::Link {
+                separators: vec![key_vals.last().unwrap().0.clone(), new_node_separator],
+                children: vec![Rc::clone(link_to_self), Rc::clone(&new_node)],
+            }
+        } else {
+            panic!("should never be called");
         }
     }
 }
@@ -66,9 +94,9 @@ impl<K: Ord + std::fmt::Debug, V: std::fmt::Debug> BPlusTree<K, V> {
         if let Some((new_separator, new_child)) = self.insert_internal(self.root.as_ref().unwrap().clone(), Rc::new(key), value) {
             let new_root = Rc::new(RefCell::new(Node::new_link()));
             if let Node::Link { separators, children } = &mut *new_root.borrow_mut() {
-                separators.push(Some(new_separator));
-                children.push(Some(Rc::clone(self.root.as_ref().unwrap())));
-                children.push(Some(new_child));
+                separators.push(new_separator);
+                children.push(Rc::clone(self.root.as_ref().unwrap()));
+                children.push(new_child);
             }
             self.root = Some(new_root);
         }
@@ -80,61 +108,46 @@ impl<K: Ord + std::fmt::Debug, V: std::fmt::Debug> BPlusTree<K, V> {
             Node::Leaf { key_vals, next, .. } => {
 
                 // if the leaf is already full then we need to make a new one and split
-                let pos = key_vals.iter().position(|maybe_filled_key| {
-                    maybe_filled_key.as_ref().map(|(k, _)| {
+                let pos = key_vals.iter().position(|(k, _)| {
                         k.as_ref() > key.as_ref()
-                    }).unwrap_or(false)
                 }).unwrap_or(key_vals.len());
 
-                key_vals.insert(pos, Some((key, value)));
+                key_vals.insert(pos, (key, value));
 
                 if key_vals.len() <= DEGREE {
                     return None;
                 }
 
-                let mid = key_vals.len() / 2; // this is ALWAYS going to be 1
-                println!("{:?}", key_vals);
-                let new_node = Rc::new(RefCell::new(Node::new_leaf()));
+                let new_link_node = node_ref.split_leaf_node(&node);
 
-                let mut new_keys_padded = key_vals.split_off(mid);
-                new_keys_padded.push(None);
+                if let Node::Link {separators, .. } = &new_link_node {
+                    return Some((Rc::clone(separators.last().unwrap()), Rc::new(RefCell::new(new_link_node))));
+                };
 
-                if let Node::Leaf { key_vals: new_keys, next: new_next, prev: new_prev } = &mut *new_node.borrow_mut() {
-                    *new_keys = new_keys_padded;
-                    *new_next = next.take();
-                    *new_prev = Some(Rc::clone(&node));
-                }
-
-                // push twice b/c we need to fill the vec
-                key_vals.push(None); key_vals.push(None);
-
-                *next = Some(Rc::clone(&new_node));
-
-                Some((key_vals[mid].take().unwrap().0.clone(), new_node))
+                return None
             }
             Node::Link { separators, children } => {
-                let pos = separators.iter().position(|maybe_k| {
-                   maybe_k.as_ref().map(|k| k.as_ref() > key.as_ref()).unwrap_or(false)
+                let pos = separators.iter().position(|k| {
+                   k.as_ref() > key.as_ref()
                 }).unwrap_or(separators.len());
+                let child = children[pos].clone();
 
-                if let Some(child) = children[pos].take() {
-                    if let Some((new_separator, new_child)) = self.insert_internal(child, key, value) {
-                        separators.insert(pos, Some(new_separator));
-                        children.insert(pos + 1, Some(new_child));
+                if let Some((new_separator, new_child)) = self.insert_internal(child, key, value) {
+                    separators.insert(pos, new_separator);
+                    children.insert(pos + 1, new_child);
 
-                        if separators.len() < DEGREE {
-                            return None;
-                        }
-
-                        let mid = separators.len() / 2;
-                        let new_node = Rc::new(RefCell::new(Node::new_link()));
-                        if let Node::Link { separators: new_separators, children: new_children } = &mut *new_node.borrow_mut() {
-                            *new_separators = separators.split_off(mid + 1);
-                            *new_children = children.split_off(mid + 1);
-                        }
-
-                        return Some((separators.pop().unwrap().unwrap(), new_node));
+                    if separators.len() < DEGREE {
+                        return None;
                     }
+
+                    let mid = separators.len() / 2;
+                    let new_node = Rc::new(RefCell::new(Node::new_link()));
+                    if let Node::Link { separators: new_separators, children: new_children } = &mut *new_node.borrow_mut() {
+                        *new_separators = separators.split_off(mid + 1);
+                        *new_children = children.split_off(mid + 1);
+                    }
+
+                    return Some((separators.pop().unwrap(), new_node));
                 }
 
                 None
@@ -167,15 +180,30 @@ mod tests {
             println!("{:?}", key_vals);
             assert_eq!(key_vals.len(), 3);
             let mut i = 0;
-            key_vals.iter().for_each(|x| {
-                match x {
-                    None => {},
-                    Some((y, _)) => {
-                        assert_eq!(y.as_ref(), &i);
-                        i += 1;
-                    }
-                }
+            key_vals.iter().for_each(|(x, _)| {
+               assert_eq!(x.as_ref(), &i);
+               i += 1;
             })
+        } else {
+            assert_eq!(true, false);
         }
+    }
+
+    #[test]
+    fn test_root_link_node() {
+        let mut tree = create_tree();
+
+        for i in 0..4 {
+            tree.insert(i, i.to_string());
+        }
+
+        let root = tree.root.as_ref().unwrap().borrow();
+        println!("{:?}", root);
+        if let Node::Link { separators, .. } = &*root {
+            println!("{:?}", separators);
+        } else {
+            assert_eq!(true, false);
+        }
+
     }
 }
