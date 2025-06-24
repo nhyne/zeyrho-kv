52afbcf1102617bcc5a4044359d692c756425f9f
nhyne <nhyne@nhyne.dev>
2025-06-24 07:28:51 -0400

    basic file wal test

--- src/queue/wal/wal.rs
+++ src/queue/wal/wal.rs
@@ -1,14 +1,12 @@
 use bytes::{Buf, BufMut, Bytes, BytesMut};
+use std::io::{Read, Write};
 
 pub struct FileWal {
-    file: std::fs::File,
+    wal_file: std::fs::File,
+    metadata_file: std::fs::File,
     uncommitted: Vec<WalEntry>,
     offset: usize,
-}
-
-struct MemoryWal {
-    data: BytesMut,
-    offset: usize,
+    size: usize,
 }
 
 struct WalEntry {
@@ -26,52 +24,68 @@ pub trait Wal {
     fn flush(&mut self) -> Result<(), std::io::Error>;
 }
 
-impl Wal for MemoryWal {
+impl Wal for FileWal {
     fn write(&mut self, record: &[u8]) -> Result<(), std::io::Error> {
-
         let entry = WalEntry {
             offset: self.offset,
             data: Bytes::from(record.to_vec()),
         };
-
-        self.data.extend_from_slice(&record);
+        self.uncommitted.push(entry);
         self.offset += record.len();
+        self.size += 1;
         Ok(())
     }
 
     fn read(&self, offset: usize) -> Result<Vec<u8>, std::io::Error> {
-        if offset + 3 > self.data.len() {
+        if offset >= self.offset {
             return Err(std::io::Error::new(
-                std::io::ErrorKind::UnexpectedEof,
+                std::io::ErrorKind::InvalidInput,
                 "Offset out of bounds",
             ));
         }
-        Ok(self.data[offset..offset + 3].to_vec())
+        let mut data = Vec::new();
+        for entry in &self.uncommitted {
+            if entry.offset == offset {
+                data.extend_from_slice(&entry.data);
+                break;
+            }
+        }
+        Ok(data)
     }
 
     fn size(&self) -> Result<usize, std::io::Error> {
-        Ok(self.data.len())
+        Ok(self.size)
     }
 
     fn flush(&mut self) -> Result<(), std::io::Error> {
-        // No-op for in-memory WAL
+        for entry in &self.uncommitted {
+            self.wal_file.write_all(&entry.data)?;
+        }
+        self.uncommitted.clear();
         Ok(())
     }
 }
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tempfile::tempfile;
 
     #[test]
-    fn test_memory_wal() {
-        let mut wal = MemoryWal {
-            data: Vec::new(),
+    fn test_file_wal() {
+        let mut wal = FileWal {
+            wal_file: tempfile().unwrap(),
+            metadata_file: tempfile().unwrap(),
+            uncommitted: Vec::new(),
             offset: 0,
+            size: 0,
         };
 
-        wal.write(entry).unwrap();
-        assert_eq!(wal.size().unwrap(), 3);
-        assert_eq!(wal.read(0).unwrap(), vec![1, 2, 3]);
+        let data = "some data goes here 100";
+        wal.write(data.as_bytes()).unwrap();
+        assert_eq!(wal.size().unwrap(), 1);
+        assert_eq!(wal.offset, data.len());
+        assert_eq!(wal.read(0).unwrap(), b"some data goes here 100");
     }
 }
